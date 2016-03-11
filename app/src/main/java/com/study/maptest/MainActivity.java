6c2947a77bc1e4b2e0b7f14f87614ed529134a7e
WXB506 <1922686887@qq.com>
2016-03-11 23:08:07 +0800

    initial commit, show baidumap succeed

---
+++ app/src/main/java/com/study/maptest/MainActivity.java
@@ -0,0 +1,44 @@
+package com.study.maptest;
+
+import android.location.LocationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
+    private BaiduMap baiduMap;
+    private LocationManager locationManager;
+    private String provider;
+    private boolean isFirstLocate = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        mapView = (MapView) findViewById(R.id.map_view);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+}
